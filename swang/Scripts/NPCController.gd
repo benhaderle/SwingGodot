6a2903ceca72b96dce16d2a1fcfb4f18edd1b9cc
Ben Haderle <ben@createneptune.com>
2025-09-18 18:09:29 -0400

    level design

--- swang/Scripts/NPCController.gd
+++ swang/Scripts/NPCController.gd
@@ -4,7 +4,7 @@ extends Area2D
 @export var dialogueLine : String
 
 func _on_body_entered(body: Node2D) -> void:
-	SignalBus.dialogue_entered.emit(dialogueLine, position - Vector2(0, $Sprite2D.get_rect().size.y * .5))
+	SignalBus.dialogue_entered.emit(dialogueLine, position - Vector2(0, $Sprite2D.region_rect.size.y * scale.y * .5))
 
 func _on_body_exited(body: Node2D) -> void:
 	SignalBus.dialogue_exited.emit()
